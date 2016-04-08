6c3e074f6f106a419aa222b7eb9e53aa6236c596
Victor Felder <victorfelder@gmail.com>
2016-04-09 00:37:59 +0200

    Better run.sh

--- 0_run.sh
+++ 0_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 MIX_ENV=prod mix compile
-MIX_ENV=prod iex --erl "-smp enable +K true +P 134217727" -S mix
+MIX_ENV=prod elixir --no-halt --erl "-smp enable +K true +P 134217727" -S mix
