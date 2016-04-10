b99fd00ea8a8be55709e1ee54005d3506f4e5111
Victor Felder <victorfelder@gmail.com>
2016-04-10 20:28:59 +0200

    Math fail

--- lib/parareq/resultlistener.ex
+++ lib/parareq/resultlistener.ex
@@ -8,22 +8,18 @@ defmodule ParaReq.ResultListener do
     for _ <- Stream.cycle([:ok]) do
       receive do
         {:tried, %{n: n, url: url}} ->
-          Cache.inc(:reqs_done)
           IO.write tried, "#{n}\t#{url}\n"
 
         {:done, %{n: n, url: url, content_type: content_type, code: code}} ->
-          Cache.inc(:reqs_done)
           IO.write good, "#{n}\t#{code}\t#{url}\t#{content_type}\n"
 
         {:error, %{n: n, url: url, reason: reason}} ->
-          Cache.inc(:reqs_done)
           if reason == "connect_timeout" do
             Cache.inc(:timeout)
           end
           IO.write error, "#{n}\t#{reason}\t#{url}\n"
 
         {:exception, %{n: n, url: url}} ->
-          Cache.inc(:reqs_done)
           IO.write exception, "#{n}\texception\t#{url}\n"
 
         after
--- lib/parareq/stats.ex
+++ lib/parareq/stats.ex
@@ -5,11 +5,11 @@ defmodule ParaReq.Pool.Stats do
   def watch do
     log = File.open!("./output/3_log", [:utf8, :read, :write, :read_ahead, :append, :delayed_write])
     for _ <- Stream.cycle([:ok]) do
-      :timer.sleep(@freq*1_000)
-      done = round(Cache.check(:reqs_done)/@freq)
+      :timer.sleep(@freq * 1_000)
+      done = round(Cache.check(:reqs_done) / @freq)
       Cache.set(:reqs_done, 0)
       alive = Cache.check(:reqs_alive)
-      rel_timeouts = round(((Cache.check(:timeout) / done) * 10000)) / 100
+      rel_timeouts = round(((Cache.check(:timeout) / @freq / done) * 10000)) / 100
       Cache.set(:timeout, 0)
       {:ok, time} = DateTime.now("Europe/Zurich") |> Timex.format("{ISO:Extended}")
       line = "#{time}\t#{done}/s\t#{alive}\t#{rel_timeouts}%"
